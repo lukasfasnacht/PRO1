313bee50ee253e3a613e386b92b9485755487e58
Lukas Fasancht <lukas.fasnacht@gmail.com>
2023-09-27 23:31:56 +0200

    added grades calc

---
+++ grades.py
@@ -0,0 +1,24 @@
+# Erstellt eine leere Liste, welche wir mit Noten füllen
+grades = []
+
+# Definiert wie viele Noten eingegeben werden können
+number_grades = int(input("Wie viele Noten möchtest du eingeben?:"))
+number = number_grades + 1
+
+# Führt die Inputs aus und checkt, ob der Input eine Zahl zwischen 1-6 ist
+for i in range(1, number):
+    while True:
+        try:
+            value = float(input(f"Geben Sie Note {i} ein: "))
+            if value <= 6:
+                grades.append(value)
+                break
+            else:
+                print("Ungültige Eingabe. Der Wert muss kleiner oder gleich 6 sein.")
+        except ValueError:
+            print("Ungültige Eingabe. Bitte geben Sie eine ganze Zahl ein.")
+
+# Rechnet den Notendurchschnitt aus
+average = sum(grades) / number_grades
+
+print(average)
