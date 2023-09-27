b17eddee9efd0d12a3107cdb326945a0cb542222
Lukas Fasancht <lukas.fasnacht@gmail.com>
2023-09-27 22:59:28 +0200

    modified flower scripts

--- adhoc_square_flower.py
+++ adhoc_square_flower.py
@@ -1,39 +1,48 @@
 import turtle
 
+# User input for different values
+font = int(input("Wie dick soll der Stift sein?:"))
+color_1 = input("Welche Farbe soll die erste Blume haben?(green, yellow, blue, lightblue etc.):")
+color_2 = input("Welche Farbe soll die zweite Blume haben?(green, yellow, blue, lightblue etc.):")
+color_3 = input("Welche Farbe soll die dritte Blume haben?(green, yellow, blue, lightblue etc.):")
+color_4 = input("Welche Farbe soll die vierte Blume haben?(green, yellow, blue, lightblue etc.):")
+color_5 = input("Welche Farbe soll die fünfte Blume haben?(green, yellow, blue, lightblue etc.):")
+size = int(input("Wie gross sollen die Dreiecke sein?:"))
+
 # Loads Turtle Environment
 screen = turtle.Screen()
 screen.title('Square Flower')
 turtle.speed(0)
 turtle.hideturtle()
-turtle.pensize(3)
+turtle.pensize(font)
 
 # Defines Square Function / Draws a perfect square including fill
-def square():
+def square(size):
   turtle.begin_fill()
-  turtle.fd(300)
+  turtle.fd(size)
   turtle.left(90)
-  turtle.fd(300)
+  turtle.fd(size)
   turtle.left(90)
-  turtle.fd(300)
+  turtle.fd(size)
   turtle.left(90)
-  turtle.fd(300)
+  turtle.fd(size)
   turtle.end_fill()
 
 # Arrangement
 turtle.right(45)
-turtle.fillcolor("lightblue")
-square()
+turtle.fillcolor(color_1)
+square(size)
 turtle.left(18)
-turtle.fillcolor("yellow")
-square()
+turtle.fillcolor(color_2)
+square(size)
 turtle.left(18)
-turtle.fillcolor("pink")
-square()
+turtle.fillcolor(color_3)
+square(size)
 turtle.left(18)
-turtle.fillcolor("blue")
-square()
+turtle.fillcolor(color_4)
+square(size)
 turtle.left(18)
-turtle.fillcolor("green")
-square()
+turtle.fillcolor(color_5)
+square(size)
 
 turtle.done()
--- adhoc_triangle_flower.py
+++ adhoc_triangle_flower.py
@@ -1,29 +1,32 @@
 import turtle
 
+# User input for different values
+font = int(input("Wie dick soll der Stift sein?:"))
+color = input("Welche Farbe soll die Blume haben?(green, yellow, blue, lightblue etc.):")
+size = int(input("Wie gross sollen die Dreiecke sein?:"))
+
 # Loads Turtle Environment
 screen = turtle.Screen()
 screen.title('Triangle Flower')
 turtle.speed(0)
 turtle.hideturtle()
-turtle.pensize(3)
+turtle.pensize(font)
 
 # Defines Triangle Function / Draws a 60degree triangle including fill
-def triangle():
+def triangle(size):
   turtle.begin_fill()
-  turtle.fd(300)
+  turtle.fd(size)
   turtle.left(120)
-  turtle.fd(300)
+  turtle.fd(size)
   turtle.left(120)
-  turtle.fd(300)
+  turtle.fd(size)
   turtle.end_fill()
 
 # Arrangement
 turtle.left(30)
-turtle.fillcolor("lightgreen")
-triangle()
-turtle.fillcolor("lightblue")
-triangle()
-turtle.fillcolor("yellow")
-triangle()
+turtle.fillcolor(color)
+triangle(size)
+triangle(size)
+triangle(size)
 
 turtle.done()
